43c445de56970207c636ddd52c7da9debde54cee
Andrii <andrii.kondratovich@gmail.com>
2024-07-17 16:39:10 +0300

    add: todos list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from "react";
+
 import "./App.css";
 // import CounterThis from "components/Counter/CounterThis";
 // import CounterAsyncLinkToElement from "components/Counter/CounterAsyncLinkToElement";
@@ -7,25 +9,62 @@ import "./App.css";
 
 // import Dropdown from "components/DropdownMenu";
 
-import colors from "dataBase/colors";
-import ColorPicker from "components/ColorPicker";
+// import colors from "dataBase/colors";
+// import ColorPicker from "components/ColorPicker";
 
-function App() {
-  return (
-    <div className="App">
-      {/* Counter */}
-      {/* <CounterThis step={1} />
-      <CounterAsyncLinkToElement step={2} />
-      <CounterStateUpd step={3} />
-      <CounterStateDefProps step={3} /> */}
+import TodoList from "components/TodoList";
+import initialTodos from "components/TodoList/todos.json";
 
-      {/* Dropdown */}
-      {/* <Dropdown /> */}
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* Counter */}
+//       {/* <CounterThis step={1} />
+//       <CounterAsyncLinkToElement step={2} />
+//       <CounterStateUpd step={3} />
+//       <CounterStateDefProps step={3} /> */}
 
-      {/* Color Picker */}
-      <ColorPicker colors={colors} />
-    </div>
-  );
+//       {/* Dropdown */}
+//       {/* <Dropdown /> */}
+
+//       {/* Color Picker */}
+//       <ColorPicker colors={colors} />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  onDeleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className="App">
+        {/* Counter */}
+        {/* <CounterThis step={1} />
+        <CounterAsyncLinkToElement step={2} />
+        <CounterStateUpd step={3} />
+        <CounterStateDefProps step={3} /> */}
+
+        {/* Dropdown */}
+        {/* <Dropdown /> */}
+
+        {/* Color Picker */}
+        {/* <ColorPicker colors={colors} /> */}
+
+        {/* Todo List */}
+        <TodoList todos={todos} deleteTodo={this.onDeleteTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
